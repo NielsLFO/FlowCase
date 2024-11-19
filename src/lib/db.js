1f6f47f50f4c2b46c91f8185bfe8651b5a9fbf6c
NielsLFO <niels.morales@lightforceortho.com>
2024-11-19 14:52:53 -0600

    cambios 19/11/24

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,13 +1,17 @@
 // db.js
 import mysql from 'mysql2/promise';
 import { createPool } from 'generic-pool';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Configuración de la conexión a la base de datos
 const dbConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Niels.Morales9086',
-    database: 'flowcase'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 };
 
 // Crear el pool de conexiones con `generic-pool`
