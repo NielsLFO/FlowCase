09c559b8ab3cb4dc51c5af72469d32eda0702e96
NielsLFO <niels.morales@lightforceortho.com>
2025-01-06 16:35:56 -0600

    Changes 6/1/25

--- src/pages/dashboard_team_lead.js
+++ src/pages/dashboard_team_lead.js
@@ -24,29 +24,33 @@ ChartJS.register(
 export default function Dashboard() {
 
     //#region Session  
-        const router = useRouter();
+    const router = useRouter();
+    const INACTIVITY_LIMIT = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
 
-        const INACTIVITY_LIMIT = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
-        
-        const handleUserActivity = () => {
+    const handleUserActivity = () => {
+        if (typeof window !== 'undefined') {
             // Update the time of the last activity
             sessionStorage.setItem('lastActivity', Date.now());
-        };
-        
-        const checkInactivity = () => {
+        }
+    };
+
+    const checkInactivity = () => {
+        if (typeof window !== 'undefined') {
             const lastActivity = sessionStorage.getItem('lastActivity');
             if (lastActivity) {
                 const currentTime = Date.now();
                 const timeSinceLastActivity = currentTime - parseInt(lastActivity, 10);
-        
+
                 // If more than 4 hours have passed, redirect to the home page
                 if (timeSinceLastActivity > INACTIVITY_LIMIT) {
                     performSignOut();
                 }
             }
-        };
-        
-        useEffect(() => {
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
             // Check if the user is authenticated
             const userEmail = sessionStorage.getItem('userEmail');
             const role = sessionStorage.getItem('roleId');
@@ -54,21 +58,23 @@ export default function Dashboard() {
                 alert(role);
                 router.push('/');
             }
-        
+
             // Set up events to track user activity
             window.addEventListener('mousemove', handleUserActivity);
             window.addEventListener('keydown', handleUserActivity);
-        
+
             // Start the interval to check for inactivity
             const interval = setInterval(checkInactivity, 60 * 1000); // Check every minute
-        
+
             // Clean up events and interval when the component unmounts
             return () => {
                 window.removeEventListener('mousemove', handleUserActivity);
                 window.removeEventListener('keydown', handleUserActivity);
                 clearInterval(interval);
             };
-        }, [router]);
+        }
+    }, [router]);
+
 
     //#endregion
 
